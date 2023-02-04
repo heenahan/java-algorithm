5523756a5ba96aa056dd9ad1e913589b432759fd
heenahan <mby1997s@gmail.com>
2023-02-04 15:55:20 +0900

    BOJ_2156

--- src/BOJ_2156/Main.java
+++ src/BOJ_2156/Main.java
@@ -24,17 +24,16 @@ public class Main {
 	}
 	
 	public int solution(int num) {
-		int[][] dp = new int[num + 1][2]; // dp[i][0] 연속 안됨 dp[i][1] 연속됨
+		// dp[i][0] 연속 안됨 dp[i][1] 연속됨 -> 연속될 경우 자신 포함할수도 안할 수도
+		int[][] dp = new int[num + 1][2];
 		if (num == 1) return amount[1];
 		dp[1][0] = amount[1]; dp[1][1] = amount[1];
-		dp[2][0] = amount[2]; dp[2][1] = dp[1][0] + amount[2];
-		int max = dp[2][1];
-		for (int i = 3; i <= num; i++) {
-			dp[i][0] = Math.max(dp[i - 2][0], dp[i - 2][1]) + amount[i];
-			dp[i][1] = dp[i - 1][0] + amount[i];
-			if (Math.max(dp[i][0], dp[i][1]) > max) max = Math.max(dp[i][0], dp[i][1]);
+		dp[2][0] = amount[2];
+		for (int i = 2; i <= num; i++) {
+			if (i > 2) dp[i][0] = Math.max(dp[i - 2][0], dp[i - 2][1]) + amount[i];
+			dp[i][1] = Math.max(dp[i - 1][0] + amount[i], dp[i - 1][1]);
 		}
-		return max;
+		return Math.max(dp[num][0], dp[num][1]);
 	}
 	
 }
