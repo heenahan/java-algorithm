55578c76bcbe801267733f59fa3e57a06171d590
heenahan <mby1997s@gmail.com>
2023-02-16 20:30:18 +0900

    BOJ_2295

--- src/BOJ_2295/Main.java
+++ src/BOJ_2295/Main.java
@@ -23,23 +23,23 @@ public class Main {
 	}
 	
 	public int solution(int num, List<Integer> arr) {
-		int max = Integer.MIN_VALUE;
 		List<Integer> two = new ArrayList<>();
-		for (int i = 0; i < num - 1; i++) {
-			for (int j = i + 1; j < num; j++) two.add(arr.get(i) + arr.get(j));
+		for (int i = 0; i < num; i++) { // x, y, z, k가 서로 같아도 됨!!
+			for (int j = i; j < num; j++) two.add(arr.get(i) + arr.get(j));
 		}
-		two = two.stream().sorted().collect(Collectors.toList());
-		for (int i = 0; i < num - 1; i++) {
-			// two + arr[i] = arr[j]
-			for (int j = i + 1; j < num; j++) {
-				int minus = arr.get(j) - arr.get(i);
+		arr = arr.stream().sorted().collect(Collectors.toList()); // 가장 큰 수가 뒤에 위치하도록
+		two = two.stream().sorted().collect(Collectors.toList()); // 이분탐색을 위한 정렬
+		for (int i = num - 1; i >= 0; i--) { // 가장 먼저 찾은 k가 문제에서 원하는 가장 큰 k
+			// two + arr[j] = arr[i]
+			for (int j = 0; j < num; j++) {
+				int minus = arr.get(i) - arr.get(j);
 				// 콜렉션에 존재한다면
 				if (Collections.binarySearch(two, minus) >= 0) {
-					if (arr.get(j) > max) max = arr.get(j);
+					return arr.get(i);
 				}
 			}
 		}
-		return max;
+		return Integer.MAX_VALUE;
 	}
 	
 	
