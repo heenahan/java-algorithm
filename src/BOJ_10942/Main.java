78f6f226d233bbf78cd5957672a1153d642a2df5
heenahan <mby1997s@gmail.com>
2024-06-20 19:38:54 +0900

    BOJ_10942

--- src/BOJ_10942/Main.java
+++ src/BOJ_10942/Main.java
@@ -1,45 +1,46 @@
 package BOJ_10942;
 
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
+
 	public static void main(String[] args) throws IOException {
-		Main T = new Main();
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int n = Integer.parseInt(br.readLine());
-		String[] input = br.readLine().split(" ");
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; i++) sb.append(input[i]);
-		int m = Integer.parseInt(br.readLine());
-		
-		T.solution(n, m, sb);
-		
-		br.close();
-		bw.close();
-	}
-	
-	public void solution(int n, int m, StringBuilder num) throws IOException {
-		boolean[][] palindrome = new boolean[n][n];
-		StringBuilder reverse = num.reverse();
+		String[] arr = br.readLine().split(" ");
+		int[] p = new int[n];
+		int[][] dp = new int[n][n];
 		for (int i = 0; i < n; i++) {
-			for (int j = i; j < n; j++) {
-				// 양쪽이 동일할 때
-				int len = j - i + 1;
-				String subNum = num.substring(i, i + len / 2);
-				String subRev = reverse.substring(n - (i + len), n - (i + len / 2 + len % 2));
-				if (subNum.equals(subRev)) palindrome[i][j] = true;
+			p[i] = Integer.parseInt(arr[i]);
+		}
+		// dp[st][ed]는 st부터 ed까지 팰린드롬인지 아닌지
+		// 길이가 1일 때 true
+		// 길이가 2일 때 양측이 동일하면 true
+		// 길이가 3이상일 때 양측이 동일하고 st + 1, ed - 1이 true이면 true
+		for (int st = n - 1; st >= 0; st--) {
+			dp[st][st] = 1;
+			for (int ed = st + 1; ed < n; ed++) {
+				if (p[st] == p[ed]) {
+					if (ed - st + 1 == 2 || dp[st + 1][ed - 1] == 1) {
+						dp[st][ed] = 1;
+						continue;
+					}
+				}
+				dp[st][ed] = 0;
 			}
 		}
+		int m = Integer.parseInt(br.readLine());
 		for (int i = 0; i < m; i++) {
-			String[] input = br.readLine().split(" ");
-			int st = Integer.parseInt(input[0]);
-			int ed = Integer.parseInt(input[1]);
-			if (palindrome[st - 1][ed - 1]) bw.write(1 + "\n");
-			else bw.write(0 + "\n");
+			String[] se = br.readLine().split(" ");
+			int s = Integer.parseInt(se[0]);
+			int e = Integer.parseInt(se[1]);
+			bw.write(String.valueOf(dp[s - 1][e - 1]));
+			bw.newLine();
 		}
+
+		br.close(); bw.close();
 	}
 }
